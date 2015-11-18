cb0931340bf4b29118da7825d59cae9fffc55f76
Janina Mueller <jamue104@pc08.pool.tp1.uni-duesseldorf.de>
2015-11-18 18:00:27 +0100

    did lab5

--- randomwalk.cxx
+++ randomwalk.cxx
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -11,6 +12,9 @@ struct colloid{
 
 void init(colloid* const c, const int N);
 void print(const colloid* const c, const int N, const string fname);
+void statistics(colloid*c, int N, double& meanx, double& meany, double& var);
+void push(colloid*c, int*rx, int*ry, int N, double step);
+void rxy(int*rx, int*ry, int N);
 
 int main(void){
   
@@ -44,9 +48,9 @@ int main(void){
     
     for(int i = 1; i <= Nfiles; i++){
 	for(int j = 0; j < Nsubsteps; j++){
-	    // call to function which randomly sets up rx and ry
-	    // call to function which pushes all colloids according to rx and ry
-	    // call to function which evaluates statistics
+	    rxy(rx,ry,N);
+	    push(c,rx,ry,N,step);
+	    statistics(c,N,meanx,meany,var);
 	    stat << (i-1)*Nsubsteps+j << "\t" << meanx << "\t";
 	    stat << meany << "\t" << var << endl;
 	}
@@ -58,6 +62,48 @@ int main(void){
     stat.close();			// close statistics file
     delete[] rx,ry,c;			// delete dynamically allocated arrays
     return 0;
+}
+
+void statistics(colloid*c, int N, double& meanx, double& meany, double& var){
+  meanx = 0;
+  meany = 0;
+  var = 0;
+  for(int i = 0; i < N; i++){
+    meanx += c[i].x;
+    meany += c[i].y;
+  }
+  meanx = (meanx/N);
+  meany = (meany/N);
+  for(int i = 0; i < N; i++){
+    var += (pow(c[i].x-meanx,2)+pow(c[i].y-meany,2));
+  }
+  var = (var/N);
+}
+
+void push(colloid*c, int*rx, int*ry, int N, double step){
+  for(int i=0; i<N; i++){
+    c[i].x += rx[i] * step;
+    c[i].y += ry[i] * step;
+  }
+}
+
+void rxy(int*rx, int*ry, int N){
+  for(int i=0; i<N; i++){
+    int a = (double(rand())/RAND_MAX + 0.5);
+    int b = (2*a)-1;
+    rx[i] = b;
+    a = (double(rand())/RAND_MAX + 0.5);
+    b = (2*a)-1;
+    ry[i] = b;
+  }
+  for(int i = 0; i < N; i++){
+    int a = (double(rand())/RAND_MAX + 0.5);
+    int b = ((2*a)-1);
+    rx[i] = b;
+    a = (double(rand())/RAND_MAX + 0.5);
+    b = ((2*a)-1);
+    ry[i] = b;
+  }
 }
 
 void init(colloid* const c, const int N){
